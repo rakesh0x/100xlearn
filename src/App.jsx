ba8668e8f6c8999f0b894fdb6efd52dd9508a262
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2024-12-18 22:36:50 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-
 import './App.css'
+import landingImg from './assets/landing.png';
 
 function App() {
+    return (
+    <>
+        <Navbar/>
+        <HeroSection/>
+    </>
+    )
+}
 
-  return (
-    fg
-  )
+const Navbar = () => {
+    return (
+        <nav id="navbar">
+            <a href='#Home'>Home</a>
+            <a href='#Quizes'>Quizes</a>
+            <a href='#Games'>Games</a>
+            <a href='#MyProfile'>MyProfile</a>
+        </nav>
+    )
+}
+
+const HeroSection = () => {
+    return (
+        <div id='hero-content'>
+            <div id='Logo'>
+                <h3>Trivia</h3>
+            </div>
+            <h1>Play, Learn, Acheive</h1>
+            <img src={landingImg} alt="landing" />
+        </div>
+    )
 }
 
 export default App
