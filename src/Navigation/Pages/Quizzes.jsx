5cc78c5963137aaba0cb837d3628669bd9d3ea13
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2025-01-27 10:57:59 +0530

    Added Routing component in sidebar and Toggle Button

--- src/Navigation/Pages/Quizzes.jsx
+++ src/Navigation/Pages/Quizzes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { useState } from "react"
 
 import {
   FaBars,
@@ -12,37 +13,48 @@ import { NavLink } from "react-router-dom";
 
 export const  Sidebar =  ({ children }) =>  {
 
+  const [ isOpen, setisOpen ] = useState(false);
+
   const menuItems = [
     { name: "Science",  icon: <FaFlask/> , path: "/sciencequiz"},
-    { name: "Mathematics", icon: <FaPlus/> ,  path: "/mathnaticsquiz"},
+    { name: "Mathematics", icon: <FaPlus/> ,  path: "/mathematicsquiz"},
     { name: "General knwoledge", icon: <FaBook/> , path: "/generalknowledgequiz"},
-  
   ]
+  const Togglesidebar = () => {
+    if(isOpen) {
+      setisOpen(!isOpen)
+    }
+
+  }
 
   return (
-    <div className="container">
-      <div className="sidebar">
-        <div className="top_section">
-          <h1>Subjects</h1>
-        </div>
-          <div className="bar">
-            <FaBars/>
+    <div className="container px-5 py-10 text-2xl" >
+      <div className="flex">
+        <div className={`sidebar ${isOpen ? "open" : ""}`}>
+            <div className="bar px-2 bg-color-white">
+              <button onClick={Togglesidebar}><FaBars/></button>
+            </div>
+              <div className="top_section py-10">
+                <h1>Subjects</h1>
+              </div>
+              {
+                menuItems.map(( item, index) => (
+                  <NavLink
+                    to={item.path}
+                    key={index}
+                    className="link"
+                    activeclassname="active"
+                  >
+                    <div key={index} className="px-2 py-3">
+                      <div className="items_icons">{item.icon}</div>
+                      <div className="link_text">{item.name}</div>
+                    </div>
+                  </NavLink>
+                ))
+              }
           </div>
-          {
-            menuItems.map(( items, index) => (
-              <NavLink
-                to={items.path}
-                key={index}
-                className="link"
-                activeClassName="link"
-              >
-                <div className="items_icons">{items.path}</div>
-                <div className="link_text">{items.name}</div>
-              </NavLink>
-            ))
-          }
-      </div>
-      <main>{ children }</main>
+          <main>{ children }</main>
+        </div>        
     </div>
   )
 }
