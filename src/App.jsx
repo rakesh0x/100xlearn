78f2afecc1d6d5a59a5c42933d4da6434116c9ff
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2025-02-20 23:16:00 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 import { Navbar } from './Navigation/Landing';
 import { LandingPage } from './Navigation/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Leaderboard } from './Navigation/Pages/Leaderboard';
 import { Sidebar } from './Navigation/Sidebar';
 import { SubjectsCard } from './DashBoard/Singleplayer';
 import { SubjectsCard2 } from './DashBoard/Singleplayer';
 import { SinglePlayer } from './Navigation/Pages/Quizzes';
+import { LiveCompetetion } from './Navigation/Pages/MultiplayerGames';
+// import { LoginForm } from './Navigation/authContext/ProtectedRoute';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
       <Navbar/>
         <div style={{ paddingTop: "3em" }}></div>
           <Routes>
             <Route path='/' element={
-                <LandingPage /> 
+                <LandingPage />
             }/>
             <Route path='/singleplayer' element = {
               <div className=' flex font-bold min-h-screen bg-gradient-to-r from-purple-600 to-blue-500 flex-grid '>
                 <div className='w-1/4 p-4'>
                    <Sidebar/>
                 </div>
-
                 <div className='w-2/4 p-4 flex'>
                    <SubjectsCard2/>
                 </div>
@@ -41,13 +40,24 @@ function App() {
               </div>
             }></Route>
             <Route path='/MultiPlayer' element={
-              <div className=' flex font-bold min-h-screen bg-gradient-to-r from-purple-600 to-blue-500 '>
+              <div className=' flex font-bold min-h-screen bg-gradient-to-b from-purple-600 to-blue-500 '>
                 <div className='w-1/4 p-4'>            
                    <Sidebar/>
                 </div>
+
+                <div className='w-2/4 p-4'>
+                   <LiveCompetetion/>
+                </div>
               </div>
               } />
-            <Route path='/leaderboard' element={<Leaderboard/>} />
+            <Route path='/leaderboard' element={
+              <div className='flex flex-col min-h-screen bg-gradient-to-b from-purple-600 to-blue-500'>
+                <div className='w-1/4 p-4'>
+                <Sidebar/>
+                </div>
+              </div>
+            }
+             />
             <Route path='/generalknowledge' element={
             <div className='flex font-seibold min-h-screen bg-gradient-to-b from-purple-600 to-blue-500 justify-center'>
               <div className='w-2/4 p-4 '>
@@ -56,9 +66,10 @@ function App() {
             </div>
             } /> 
           </Routes>
-      </BrowserRouter>
     </>
   );
 }
 
 export default App;
+
+
