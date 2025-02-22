71d6e248527d4d25786e2dcadf3a5247e890bdea
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2025-02-22 19:14:08 +0530

    Deployment issue

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = 'dev-b1i5482fgz51t3c.us.auth0.com';
-const clientId = '9x8EaRkzklgNDYNvzWoSqauRrPKEAABC';
+const domain = "dev-b1i5482fgz51t3c.us.auth0.com";
+const clientId = "9x8EaRkzklgNDYNvzWoSqauRrPKEAABC";
 
 const root = createRoot(document.getElementById("root"));
 
@@ -14,7 +14,7 @@ root.render(
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: window.location.origin }}
+      authorizationParams={{ redirect_uri: "http://localhost:5173/callback" }}
     >
       <App />
     </Auth0Provider>
