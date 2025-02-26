018458b6a2b4cb02ee323f68c8171d77a5609da2
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2025-02-26 19:28:01 +0530

    Migrated to HTTPS

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }, server: {
+  },
+  server: {
+    https: true, // Enable HTTPS
     headers: {
-      "Cross-origin-opener-Policy" : "same-origin-allow-popups",
-      "Cross-origin-Embedder-Policy" : "require-core"
-    }
-  }
-})
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Embedder-Policy": "require-core",
+    },
+  },
+});
