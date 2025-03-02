01c52054286ffdda9d6953069bd71912cf5591ad
rakeshcodes123 <178692736+rakeshcodes123@users.noreply.github.com>
2025-03-02 09:04:32 +0530

    added some pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import  App  from "./App"
+import  App from "./App"
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
 const clientId = "1025249650061-vb3fpofqdq4h9onak5q4df396ksb76ji.apps.googleusercontent.com"
